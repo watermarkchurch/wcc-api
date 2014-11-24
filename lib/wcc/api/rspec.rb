0f514b24496ac8399e29dbca3837742914149171
Travis Petticrew <travis@petticrew.net>
2014-11-24 08:18:07 -0600

    Added CollectionMatchers module for specs
    A convenient RSpec macro for matching collections

---
+++ lib/wcc/api/rspec.rb
@@ -0,0 +1,3 @@
+require 'wcc/api/rspec/pagination_examples'
+require 'wcc/api/rspec/collection_matchers'
+
---
+++ lib/wcc/api/rspec/collection_matchers.rb
@@ -0,0 +1,13 @@
+module WCC::API::RSpec
+  module CollectionMatchers
+    def collection_match(coll1, coll2, matcher = :eq)
+      coll1, coll2 = coll1.to_a, coll2.to_a
+      expect(coll1.size).to eq(coll2.size)
+
+      coll1.zip(coll2).each do |actual, expected|
+        expect(actual).to public_send(matcher, expected)
+      end
+    end
+  end
+end
+
