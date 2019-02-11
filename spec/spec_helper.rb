bfa12d70bc2b42c6e72410e68c43f66ff2913b87
Justin Powell <jpowellcs@gmail.com>
2019-02-11 12:13:09 -0600

    add rest client specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,29 @@
-SPEC_DIR = File.dirname(__FILE__)
-FIXTURES_DIR = File.join(SPEC_DIR, "fixtures")
+# frozen_string_literal: true
 
-$LOAD_PATH.unshift File.join(SPEC_DIR, "..", "lib")
+SPEC_DIR = File.dirname(__FILE__)
+FIXTURES_DIR = File.join(SPEC_DIR, 'fixtures')
+
+$LOAD_PATH.unshift File.join(SPEC_DIR, '..', 'lib')
 $LOAD_PATH.unshift SPEC_DIR
 
 require 'dotenv'
 Dotenv.load
 
+require 'webmock/rspec'
+Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
+
 require 'wcc/api'
 
-Dir[File.join(SPEC_DIR, "support", "*.rb")].each do |support_file|
-  require "support/#{File.basename(support_file, ".rb")}"
-end
-
 RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
 
-  config.order = 'random'
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.include FixturesHelper
 end
